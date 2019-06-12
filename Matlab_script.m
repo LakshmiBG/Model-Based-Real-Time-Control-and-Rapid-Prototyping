a585d1b3acf9d6e4bcc54eb5a7daf5ae1bf590cd
LakshmiBG <51742367+LakshmiBG@users.noreply.github.com>
2019-06-12 12:54:16 +0300

    Add files via upload

---
+++ Matlab_script.m
@@ -0,0 +1,10 @@
+SampleTime = 0.002; 
+
+%Joint velcoity mode
+max_velocity_1 = 14;                % Joint q1 velocity gain
+max_velocity_2 = 15;                % Joint q2 velocity gain
+max_velocity_3 = 13;                % Joint q3 velocity gain
+max_velocity_4 = 100;               % Telescopic Joint d4 velocity gain
+
+%Planar tip mode
+XY_vel = 0.5;                       % Planar mode velocity gain
